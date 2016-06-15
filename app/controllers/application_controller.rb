bd9b248fc0a03203700e30728a6ad6da1c683aed
Paul Chobert <paul@chobert.fr>
2016-06-15 11:10:54 +0200

    Add global password protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: 'dataexpeditions', password: ENV.fetch('PASSWORD') if ENV['PASSWORD']
+
   protect_from_forgery with: :exception
 
   layout :layout_by_resource
